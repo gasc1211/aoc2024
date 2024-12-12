d49c798379eb897e8d25ab9c1c9964f85ddaff73
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-12-11 22:12:23 -0600

    Solution day 11 v2

--- Day 11: Plutonian Pebbles/index.ts
+++ Day 11: Plutonian Pebbles/index.ts
@@ -1,33 +1,39 @@
 // Day 11: Plutonian Pebbles
 
-let pebbles = "475449 2599064 213 0 2 65 5755 51149";
+let input = "475449 2599064 213 0 2 65 5755 51149";
 // let pebbles: string[] = "125 17";
+
+let pebbles: number[] = input.split(" ").map(pebble => parseInt(pebble));
 const blinks = 25;
 
-function pebble_slicer(input: string){
-  pebbles = input.split(" ")
-  let new_pebbles = []
-  pebbles.forEach(pebble => {
-    if (pebble === "0") {
+function pebble_slicer(input: number[]) {
+  let new_pebbles: number[] = [];
+  input.forEach(pebble => {
+    if (pebble === 0) {
       new_pebbles.push(1)
-    } else if (pebble.length % 2 === 0) {
-      let sub_pebbles = [pebble.slice(0, pebble.length / 2), pebble.slice(pebble.length / 2)];
+    } else if (digits(pebble) % 2 === 0) {
+      let _pebble = pebble.toString();
+      let sub_pebbles = [_pebble.slice(0, _pebble.length / 2), _pebble.slice(_pebble.length / 2)];
       sub_pebbles.forEach(sub_pebble => {
         new_pebbles.push(parseInt(sub_pebble));
       });
     } else {
-      new_pebbles.push(parseInt(pebble) * 2024);
+      new_pebbles.push(pebble * 2024);
     }
   });
-  return new_pebbles.join(" ");
+  return new_pebbles;
+}
+
+function digits(n: number) {
+  return Math.floor(Math.log10(n) + 1);
 }
 
 console.log("Initial arrangement: ");
-console.log(pebbles);
+console.log(input);
 
 for (let i = 0; i < blinks; i++) {
   pebbles = pebble_slicer(pebbles);
-  console.log(`\nAfter ${i+1} blinks: `);
+  console.log(`\nAfter ${i + 1} blinks: `);
   console.log(pebbles);
   console.log(`Length:${pebbles.length}`);
 }
